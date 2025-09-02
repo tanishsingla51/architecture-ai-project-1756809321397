bf2b4d3bcd399666cdd31be9e5ef8d5acd133824
Tanish Singla <131950694+tanishsingla51@users.noreply.github.com>
2025-09-02 16:05:34 +0530

    Add src/routes/auth.routes.js

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { body } = require('express-validator');
+const authController = require('../controllers/auth.controller');
+const validate = require('../middleware/validate');
+
+const router = express.Router();
+
+router.post(
+  '/register',
+  validate([
+    body('username').isString().notEmpty().withMessage('Username is required'),
+    body('email').isEmail().withMessage('Please provide a valid email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+  ]),
+  authController.register
+);
+
+router.post(
+  '/login',
+  validate([
+    body('email').isEmail().withMessage('Please provide a valid email'),
+    body('password').notEmpty().withMessage('Password is required'),
+  ]),
+  authController.login
+);
+
+module.exports = router;
